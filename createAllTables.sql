156f1a8bd943f915ecbb1c76c1e5768ec2cfd84c
jasonbohlinger <jbohlinger@chapman.edu>
2023-04-25 13:12:35 -0700

    removed unnecessary code. only includes table create queries now

--- createAllTables.sql
+++ createAllTables.sql
@@ -11,53 +11,79 @@ CREATE TABLE directors(
 );
 
 CREATE TABLE movies(
-    movieID INTEGER PRIMARY KEY NOT NULL,
+    movieID VARCHAR(20) PRIMARY KEY NOT NULL,
     directorID INTEGER,
     title VARCHAR(120),
     description VARCHAR(1000),
     genre VARCHAR(20),
     runTime INTEGER, -- MINUTES
-    releaseYear INTEGER
+    releaseYear INTEGER,
+    imdbScore DECIMAL(2, 1),
     FOREIGN KEY (directorID) REFERENCES directors(directorID)
 );
 
 -- table for M-M relationship from movies to actors
 CREATE TABLE movieActors(
-    movieID INTEGER PRIMARY KEY NOT NULL,
-    actorID INTEGER PRIMARY KEY NOT NULL
+    movieID VARCHAR(20) NOT NULL,
+    actorID INTEGER NOT NULL,
+    PRIMARY KEY (movieID, actorID)
 );
 -- table for M-M relationship from tvShows to actors
 CREATE TABLE tvShowActors(
-    tvShowID INTEGER PRIMARY KEY NOT NULL,
-    actorID INTEGER PRIMARY KEY NOT NULL
+    tvShowID VARCHAR(20) NOT NULL,
+    actorID INTEGER NOT NULL,
+    PRIMARY KEY (tvShowID, actorID)
 );
 
 CREATE TABLE tvShows(
-    tvShowID INTEGER PRIMARY KEY NOT NULL,
+    tvShowID VARCHAR(20) PRIMARY KEY NOT NULL,
     directorID INTEGER,
     title VARCHAR(120),
-    imdbScore INTEGER,
     genre VARCHAR(15),
     runTime INTEGER, -- MINUTES
     releaseYear INTEGER,
     seasons INTEGER,
+    imdbScore DECIMAL(2, 1),
     FOREIGN KEY (directorID) REFERENCES directors(directorID)
 );
 
 CREATE TABLE reviews(
-    reviewID INTEGER PRIMARY KEY NOT NULL,
+    reviewID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     writtenReviews VARCHAR(256),
-    tvShowID INTEGER
-    movieID INTEGER,
+    tvShowID VARCHAR(20),
+    movieID VARCHAR(20),
     userID INTEGER,
-    userRating INTEGER
-    FOREIGN KEY (tvShowID) REFERENCES tvShows(tvShowID)
-    FOREIGN KEY (movieID) REFERENCES movies(movieID)
+    userRating INTEGER,
+    FOREIGN KEY (tvShowID) REFERENCES tvShows(tvShowID),
+    FOREIGN KEY (movieID) REFERENCES movies(movieID),
     FOREIGN KEY (userID) REFERENCES users(userID)
-
 );
 
 CREATE TABLE users(
-    userID INTEGER PRIMARY KEY NOT NULL,
+    userID INTEGER PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(20)
+);
+
+CREATE TABLE titlesWhole(
+    id VARCHAR(20),
+    title VARCHAR(120),
+    type VARCHAR(10),
+    description VARCHAR(1500),
+    release_year INTEGER,
+    age_certification VARCHAR(10),
+    runtime INTEGER,
+    genre VARCHAR(30),
+    production_country VARCHAR(5),
+    seasons INTEGER,
+    imdb_id VARCHAR(20),
+    imdb_score DECIMAL(2, 1),
+    imdb_votes INTEGER
+);
+
+CREATE TABLE creditsWhole(
+    person_id INTEGER,
+    id VARCHAR(20),
+    name VARCHAR(40),
+    character_name VARCHAR(500),
+    role VARCHAR(10)
 );
